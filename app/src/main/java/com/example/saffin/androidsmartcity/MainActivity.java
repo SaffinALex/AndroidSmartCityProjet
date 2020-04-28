c3ba7dd878239de8cc87dfd6cbb84d11f7859244
SaffinALex <alexsaffin@free.fr>
2020-04-28 11:22:27 +0200

    Inscription

--- app/src/main/java/com/example/saffin/androidsmartcity/MainActivity.java
+++ app/src/main/java/com/example/saffin/androidsmartcity/MainActivity.java
@@ -11,7 +11,10 @@ package com.example.saffin.androidsmartcity;
         import android.os.Bundle;
         import android.view.View;
 
+        import com.firebase.ui.auth.AuthUI;
+
         import java.io.IOException;
+        import java.util.Arrays;
 
 public class MainActivity extends Activity {
 
@@ -21,13 +24,35 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_reception_screen);
     }
 
+    //FOR DATA
+    // 1 - Identifier for Sign-In Activity
+    private static final int RC_SIGN_IN = 123;
+
+    // --------------------
+    // NAVIGATION
+    // --------------------
+
+    // 2 - Launch Sign-In Activity
+    private void startSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                        new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
     public void goIdentification(View v) throws IOException {
         Intent intent = new Intent(MainActivity.this,Connexion.class);
         startActivity(intent);
     }
     public void goCreateAccount(View v) throws IOException {
-        Intent intent = new Intent(MainActivity.this,AccountCreation.class);
-        startActivity(intent);
+        this.startSignInActivity();
     }
 
 }
